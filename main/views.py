d4b083efb185e1f742fedec084d77fb1c2e1a579
SoftwareSensei <softwaresensei74@gmail.com>
2025-02-08 18:50:50 +0000

    my commit

--- main/views.py
+++ main/views.py
@@ -57,7 +57,7 @@ def login_view(request):
                 messages.error(request, 'User not found, invalid username or password.')
         else:
             messages.error(request, 'User not found, invalid username or password.')
-            return render(request, 'list.html', {'form': form})
+            return render(request, 'checkout.html', {'form': form})
     else:
         form = AuthenticationForm()
         return render(request, 'login.html', {'form': form})
