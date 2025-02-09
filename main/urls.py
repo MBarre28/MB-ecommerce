bec4b0fc34a347ba9bbe70970cff2c8685191ed0
SoftwareSensei <softwaresensei74@gmail.com>
2025-02-09 11:19:29 +0000

    my commit

--- main/urls.py
+++ main/urls.py
@@ -5,20 +5,26 @@ from .views import (ProductListView, ProductDetailView,
                      order_history, login_view, logout_view, register)
 
 urlpatterns = [
+
+    # Product endpoints URL pattern
     path('', ProductListView.as_view(), name='product_list'),
     path('categories/', CategoryListView.as_view(), name='categories_list'),
     path('category/<slug:category_slug>/', ProductListView.as_view(), name='category_products'),
     path('product/<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+
+    # Cart endpoints URL pattern
     path('cart/add/<int:product_id>/', cart_add, name='cart_add'),
     path('cart/remove/<int:product_id>/', cart_remove, name='cart_remove'),
     path('cart/update/<int:product_id>/', cart_update, name='cart_update'),
     path('cart/', cart_detail, name='cart_detail'),
+
     path('checkout/', checkout, name='checkout'),
 
+    # Order endpoints URL pattern
     path('order/confirmation/<int:order_id>/', order_confirmation, name='order_confirmation'),
     path('order/history', order_history, name='order_history'),
 
-
+    # User endpoints URL pattern
     path('register/', register, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
