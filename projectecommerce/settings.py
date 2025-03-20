69a3cf780d3a444b00642eb4f4a705e3a34f072b
SoftwareSensei <softwaresensei74@gmail.com>
2025-03-20 07:23:22 +0000

    my commit

--- projectecommerce/settings.py
+++ projectecommerce/settings.py
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = "login"
 
-LOGIN_REDIRECT_URL = "checkout"
+LOGIN_REDIRECT_URL = "product_list"
 LOGOUT_REDIRECT_URL = "product_list"
 
 # paypal payment settings
