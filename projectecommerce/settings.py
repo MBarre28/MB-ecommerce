abcb16b384ef9b838d2adac5f153a4c332c5d9fd
SoftwareSensei <softwaresensei74@gmail.com>
2025-04-15 10:57:36 +0100

    my commit

--- projectecommerce/settings.py
+++ projectecommerce/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/app/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "app/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
